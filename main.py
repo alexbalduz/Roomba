8df4b3a5f8e71e60a6562987f37f236fd12e538f
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-02-24 09:02:44 +0000

    funcionarios crear nueva ventana

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
-import secundaria
 import alimpiar
 import lista
 import diccionarios
 import script
 import tupla
 
-label = tk.Label(text="Welcome to the Seven Heaven")
+def abrirventana2():
+    ventana.withdraw()
+    win= tk.Toplevel()
+    win.geometry('380x300+1900+100')
+    win.title('Dimensiones')
+    e3=tk.Label(win, text='Bienvenido a la segunda ventana')
+    e3.pack(padx=5,pady=5,ipadx=5,ipady=5,fill=tk.X)
+    boton2=tk.Button(win,text='OK', command=win.destroy)
+    boton2.pack(side=tk.TOP)
+
+def cerrarventana():
+    ventana.destroy()
+
+ventana = tk.Tk()
+ventana.title('Welcome to Roomba')
+ventana.geometry('380x300')
+label = tk.Label(text="Introduzca las zonas a limpiar:")
 label.pack()
+
+combo = ttk.Combobox(ventana)
+
+combo['values']= (0, 1, 2, 3, 4)
+
+combo.current(0) #set the selected item
+
+combo.grid(column=0, row=0)
+
+button =tk.Button(ventana, text = 'OK', command = abrirventana2)
+button.pack()
+ventana.mainloop()
 
 
 
